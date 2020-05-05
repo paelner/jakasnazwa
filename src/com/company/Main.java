40b44f3a310c2898b19bd4acd7153b08b73c6533
aegrshdtjfku <songoku.nerkowski3@gmail.com>
2020-05-05 11:44:28 +0200

    zadanie 0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,18 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
         Human me = new Human();
-        me.firstName = "Jacek";
-        me.lastName = "Placek";
-        me.pet = new Animal("mouse");
-        me.pet.name = "Myszojeleń";
-        me.pet.feed();
-
-        System.out.println(me.pet.name);
+        me.firstName = "Paweł";
+        me.lastName = "Nerkowski";
+        me.pet = new Animal("moose");
+        me.pet.name = "Kłębek";
 
         Animal dog = new Animal("dog");
         dog.name = "Akita";
         me.pet = dog;
-
-        me.pet.feed();
-        me.pet.feed();
-        me.pet.feed();
-
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        me.pet.takeForAWalk();
-//        for (int i = 0; )
-//        do{
-//            me.pet.takeForAWalk();
-//        }while (me.pet.imDead());
-
-        me.car = new Car("Volkswagen","Passat",1984,1.9,"Red");
-
-        System.out.println("test");
     }
 }
